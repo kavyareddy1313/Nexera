b5bc60465d0380ba678ae9b8c5f8027b2a866c11
kavyareddy1313 <kavyareddy1313@users.noreply.github.com>
2026-08-07 09:14:27 +0530

    feat(pages): create InstructorCalendarPage

---
+++ Frontend/src/pages/InstructorCalendarPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { InstructorCalendar } from '../components/instructor/InstructorCalendar';
+
+export default function InstructorCalendarPage() {
+  return <InstructorCalendar />;
+}
