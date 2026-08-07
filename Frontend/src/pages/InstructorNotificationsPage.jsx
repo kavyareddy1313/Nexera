c7223d933144babea2f85a1a7be36480020c7817
kavyareddy1313 <kavyareddy1313@users.noreply.github.com>
2026-08-07 09:14:27 +0530

    feat(pages): create InstructorNotificationsPage

---
+++ Frontend/src/pages/InstructorNotificationsPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { InstructorNotifications } from '../components/instructor/InstructorNotifications';
+
+export default function InstructorNotificationsPage() {
+  return <InstructorNotifications />;
+}
