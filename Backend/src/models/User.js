5ae70e6bb2644bcd412962cb791aef96356bfaef
kavyareddy1313 <kavyareddy1313@users.noreply.github.com>
2026-08-07 09:14:16 +0530

    feat(models): create User Sequelize model with password hashing and tokens

---
+++ Backend/src/models/User.js
@@ -0,0 +1,76 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../config/db.js';
+import bcrypt from 'bcryptjs';
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  fullName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'full_name'
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  avatarUrl: {
+    type: DataTypes.STRING,
+    field: 'avatar_url'
+  },
+  isOnline: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+    field: 'is_online'
+  },
+  emailVerified: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+    field: 'email_verified'
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'student',
+    validate: {
+      isIn: [['student', 'instructor', 'admin']]
+    }
+  },
+}, {
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        user.password = await bcrypt.hash(user.password, 12);
+      }
+    },
+    beforeUpdate: async (user) => {
+      if (user.changed('password')) {
+        user.password = await bcrypt.hash(user.password, 12);
+      }
+    },
+  },
+  underscored: true,
+  tableName: 'Users'
+});
+
+User.prototype.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+export default User;
