cb6ab0b0bd7883a973a25d31611214e725eeb8bf
kavyareddy1313 <kavyareddy1313@users.noreply.github.com>
2026-08-07 09:14:27 +0530

    feat(pages): create InstructorStudentsPage

---
+++ Frontend/src/pages/InstructorStudentsPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { InstructorStudents } from '../components/instructor/InstructorStudents';
+
+export default function InstructorStudentsPage() {
+  return <InstructorStudents />;
+}
