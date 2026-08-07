27986fbec0f674a7bbf37ab8b09c3124f97db5b5
kavyareddy1313 <kavyareddy1313@users.noreply.github.com>
2026-08-07 09:14:27 +0530

    feat(pages): create InstructorSettingsPage

---
+++ Frontend/src/pages/InstructorSettingsPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { InstructorSettings } from '../components/instructor/InstructorSettings';
+
+export default function InstructorSettingsPage() {
+  return <InstructorSettings />;
+}
