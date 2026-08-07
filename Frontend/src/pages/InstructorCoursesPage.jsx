90b411f57abef0fcf203be34ff7cd952ce733042
kavyareddy1313 <kavyareddy1313@users.noreply.github.com>
2026-08-07 09:14:26 +0530

    feat(pages): create InstructorCoursesPage for managing courses

---
+++ Frontend/src/pages/InstructorCoursesPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { InstructorCourses } from '../components/instructor/InstructorCourses';
+
+export default function InstructorCoursesPage() {
+  return <InstructorCourses />;
+}
