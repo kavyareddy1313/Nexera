fa6ce8062724cd12e9cc90e85c65b975d8b0a455
kavyareddy1313 <kavyareddy1313@users.noreply.github.com>
2026-08-07 09:14:27 +0530

    feat(pages): create InstructorAnalyticsPage

---
+++ Frontend/src/pages/InstructorAnalyticsPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { InstructorAnalytics } from '../components/instructor/InstructorAnalytics';
+
+export default function InstructorAnalyticsPage() {
+  return <InstructorAnalytics />;
+}
