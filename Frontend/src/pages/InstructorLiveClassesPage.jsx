8f41488d64d8c69e442e77eaa2a9a425f803763f
kavyareddy1313 <kavyareddy1313@users.noreply.github.com>
2026-08-07 09:14:26 +0530

    feat(pages): create InstructorLiveClassesPage

---
+++ Frontend/src/pages/InstructorLiveClassesPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { InstructorLiveClasses } from '../components/instructor/InstructorLiveClasses';
+
+export default function InstructorLiveClassesPage() {
+  return <InstructorLiveClasses />;
+}
