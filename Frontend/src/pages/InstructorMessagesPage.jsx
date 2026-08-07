a6630fa54e7f8affc4458caa7e051633b2f86613
kavyareddy1313 <kavyareddy1313@users.noreply.github.com>
2026-08-07 09:14:27 +0530

    feat(pages): create InstructorMessagesPage

---
+++ Frontend/src/pages/InstructorMessagesPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { InstructorMessages } from '../components/instructor/InstructorMessages';
+
+export default function InstructorMessagesPage() {
+  return <InstructorMessages />;
+}
